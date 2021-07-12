d33295ff9b0f1ae756feee6d686bd0b593d450d7
DH Min <hyuki0920@gmail.com>
2021-07-12 23:19:29 +0900

    first comment

---
+++ src/test/java/io/dhmin/test/TestClass.java
@@ -0,0 +1,11 @@
+package io.dhmin.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    @Test
+    void alwaysHappy() {
+        Assertions.assertTrue(true);
+    }
+}
